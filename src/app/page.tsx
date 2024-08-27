f577b81bcecfc3955633bc3ce8de54eb56c9e2ac
sevda <sevda.m.anefi@gmail.com>
2024-08-27 20:55:26 +0300

    add mockdata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
+import mockData from "../mockData.json";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Gallery in progress
+      <div className="flex flex-wrap">
+        {mockData.map((item) => (
+          <img
+            key={item.key}
+            src={item.url}
+            alt={item.title}
+            className="w-1/4"
+          />
+        ))}
+      </div>
     </main>
   );
 }
