586cb34c09a69ee372431e67f7b5224e7e094c6d
sevda <sevda.m.anefi@gmail.com>
2024-08-27 21:57:11 +0300

    add db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import Link from "next/link";
-import mockData from "../mockData.json";
+import { db } from "../server/db/index";
 
-export default function HomePage() {
+// export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.createdAt),
+  });
+
+  console.log(images);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="flex flex-wrap">
-        {mockData.map((item) => (
-          <img
-            key={item.key}
-            src={item.url}
-            alt={item.title}
-            className="w-1/4"
-          />
+        {images.map((item) => (
+          <div key={item.url} className="flex w-48 flex-col">
+            <img src={item.url} alt={item.name} />
+            <div>{item.name}</div>
+          </div>
         ))}
       </div>
     </main>
